606d504f029233277d09d706d59ccb84550f9c27
MohammadAmin Khataei <m.khatayi@tech1a.co>
2023-06-26 09:41:10 +0330

    host check

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 """_summary_
 """
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from config import settings
 from routers.marketer import marketer
